e5cbae0edba10a667209dc7f639e40c53361de03
202530 <dgladushenko@mail.ru>
2021-10-22 01:14:08 +0300

    Add raz/raza and picture

--- app/src/main/java/com/example/project_2/MainActivity.java
+++ app/src/main/java/com/example/project_2/MainActivity.java
@@ -12,6 +12,8 @@ public class MainActivity extends AppCompatActivity {
     TextView mainText;
     Button mainBtn;
     Button mainBtn2;
+    Button mainBtn3;
+
     private int score = 0;
 
     @Override
@@ -24,22 +26,47 @@ public class MainActivity extends AppCompatActivity {
 
         mainBtn2 = (Button) findViewById(R.id.main_btn2);
         mainBtn2.setOnClickListener(clickListener2);
+
+        mainBtn3 = (Button) findViewById(R.id.main_btn3);
+        mainBtn3.setOnClickListener(clickListener3);
     }
 
     View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            score ++;
-            String s = "Кликов: " + score;
-            mainText.setText(s.toCharArray(),0, s.length());
+            score++;
+            int ost = score % 10;
+            int osta = score % 100;
+            if (((ost > 1 && ost < 5) || (ost < -1 && ost > -5)) && osta != 12 && osta != 13 && osta != 14) {
+                String s = "Кнопка нажата: " + score + " раза";
+                mainText.setText(s.toCharArray(), 0, s.length());
+            } else {
+                String s = "Кнопка нажата: " + score + " раз";
+                mainText.setText(s.toCharArray(), 0, s.length());
+            }
         }
     };
     View.OnClickListener clickListener2 = new View.OnClickListener() {
         @Override
         public void onClick(View view) {
-            score --;
+            score--;
+            int ost = score % 10;
+            int osta = score % 100;
+            if (((ost > 1 && ost < 5) || (ost < -1 && ost > -5)) && osta != 12 && osta != 13 && osta != 14) {
+                String s = "Кнопка нажата: " + score + " раза";
+                mainText.setText(s.toCharArray(), 0, s.length());
+            } else {
+                String s = "Кнопка нажата: " + score + " раз";
+                mainText.setText(s.toCharArray(), 0, s.length());
+            }
+        }
+        };
+    View.OnClickListener clickListener3 = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            score = 0;
             String s = "Кликов: " + score;
-            mainText.setText(s.toCharArray(),0, s.length());
+            mainText.setText(s.toCharArray(), 0, s.length());
         }
     };
 }

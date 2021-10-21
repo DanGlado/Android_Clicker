2e6458e7ae26408e26b32e5cfe9ad57d0b22e949
202530 <dgladushenko@mail.ru>
2021-10-21 23:44:33 +0300

    + Add minus button

--- app/src/main/java/com/example/project_2/MainActivity.java
+++ app/src/main/java/com/example/project_2/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView mainText;
     Button mainBtn;
-
+    Button mainBtn2;
     private int score = 0;
 
     @Override
@@ -22,12 +22,22 @@ public class MainActivity extends AppCompatActivity {
         mainBtn = (Button) findViewById(R.id.main_btn);
         mainBtn.setOnClickListener(clickListener);
 
+        mainBtn2 = (Button) findViewById(R.id.main_btn2);
+        mainBtn2.setOnClickListener(clickListener2);
     }
 
     View.OnClickListener clickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
             score ++;
+            String s = "Кликов: " + score;
+            mainText.setText(s.toCharArray(),0, s.length());
+        }
+    };
+    View.OnClickListener clickListener2 = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+            score --;
             String s = "Кликов: " + score;
             mainText.setText(s.toCharArray(),0, s.length());
         }
